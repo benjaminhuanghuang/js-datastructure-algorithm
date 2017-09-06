f0665acf7ad0ddc409721b702f7a2fb1eae13725
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-06 11:59:24 -0700

    udpate

--- fibonacci.js
+++ fibonacci.js
@@ -5,4 +5,13 @@ function fibonacci(position){
     return fibonacci(position - 1) + fibonacci(position - 2);
 }
 
-module.exports = fibonacci;
+function fibonacci_dp(position) {
+  var f = [];
+  f[0] = 0;
+  f[1] = 1;
+  for (var i = 2; i <= position; i++)
+        f[i] = f[i-1] + f[i-2];
+  return f[position];
+}
+
+module.exports = { fibonacci, fibonacci_dp};
--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var fibonacci = require('../fibonacci');
+var { fibonacci, fibonacci_dp} = require('../fibonacci');
 
 describe('fibonacci: ', () => {
   it('return 1 at position 1', () => {
@@ -18,5 +18,41 @@ describe('fibonacci: ', () => {
     const position = 9;
     const res = fibonacci(position);
     assert.equal(res, 34);
+  });
+
+  it('execution time when position 50', () => {
+    const position = 50;
+    const now = Date.now();
+    const res = fibonacci(position);
+    assert.equal(res, 12586269025);
+    console.log(Date.now() - now);
+  });
+});
+
+describe('fibonacci_dp: ', () => {
+  it('return 1 at position 1', () => {
+    const position = 1;
+    const res = fibonacci_dp(position);
+    assert.equal(res, 1);
+  });
+
+  it('return 3 at position 4', () => {
+    const position = 4;
+    const res = fibonacci_dp(position);
+    assert.equal(res, 3);
+  });
+
+  it('return 34 at position 9', () => {
+    const position = 9;
+    const res = fibonacci_dp(position);
+    assert.equal(res, 34);
+  });
+
+  it('execution time when position 50', () => {
+    const position = 50;
+    const now = Date.now();
+    const res = fibonacci_dp(position);
+    assert.equal(res, 12586269025);
+    console.log(Date.now() - now);
   });
 });
