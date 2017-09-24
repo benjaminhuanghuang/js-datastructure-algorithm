6b9aa627e5549779372055a408148097709e784e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-24 00:07:44 -0700

    update

--- leetcode.tests/234.test.js
+++ leetcode.tests/234.test.js
@@ -9,10 +9,30 @@ describe("234. Palindrome Linked List", () => {
     assert.isFalse(res);
   });
 
+  it(`return false for [1,0,0]`, () => {
+    let input = buildList([1,0,0]);
+    const res = isPalindrome(input);
+    assert.isFalse(res);
+  });
+
   it(`reverseLinkedList [1,2,4,5,6,7,8]`, () => {
     let input = buildList([1,2,4,5,6,7,8]);
     const res = reverseLinkedList(input, 3);
     const values = getListValues(res);
     assert.deepEqual(values, [8,7,6,5]);
+  });
+
+  it(`reverseLinkedList [1,2]`, () => {
+    let input = buildList([1,2]);
+    const res = reverseLinkedList(input, 1);
+    const values = getListValues(res);
+    assert.deepEqual(values, [2]);
+  });
+
+  it(`reverseLinkedList [1,0,0]`, () => {
+    let input = buildList([1,0,0]);
+    const res = reverseLinkedList(input, 2);
+    const values = getListValues(res);
+    assert.deepEqual(values, [0]);
   });
 });
--- leetcode/234.PalindromeLinkedList.js
+++ leetcode/234.PalindromeLinkedList.js
@@ -9,7 +9,9 @@ function ListNode(val) {
 var isPalindrome = function(head) {
   if (head == null) return true;
   const linkLength = getLinkLength(head);
-  const half = reverseLinkedList(head, Math.ceil(linkLength / 2.0) + 1);
+  //let count = linkLength % 2 ? linkLength / 2 + 1 : linkLength / 2;
+  let count = Math.ceil(linkLength / 2 );
+  const half = reverseLinkedList(head, count);
 
   while (head && half) {
     if (head.val != half.val) return false;
@@ -32,6 +34,7 @@ function reverseLinkedList(head, startIndex) {
   let halfHeader = head;
   while (startIndex > 0 && halfHeader) {
     halfHeader = halfHeader.next;
+    startIndex--;
   }
 
   let prev = null;
@@ -41,7 +44,25 @@ function reverseLinkedList(head, startIndex) {
     prev = halfHeader;
     halfHeader = next;
   }
-  return halfHeader;
+  return prev;
 }
 
 module.exports = { isPalindrome, reverseLinkedList };
+
+function buildList(nums) {
+  if (nums == null || nums.length == 0) {
+    return null;
+  }
+  let dummyHeader = new ListNode(-1);
+  let curr = dummyHeader;
+  for (num of nums) {
+    curr.next = new ListNode(num);
+    curr = curr.next;
+  }
+  return dummyHeader.next;
+}
+
+let input = buildList([1,0,0]);
+res = isPalindrome(input);
+
+console.log(3/2);
