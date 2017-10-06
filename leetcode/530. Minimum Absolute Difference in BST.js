8d1f03b423f9a87a83f56ea8a07e4cc45d8790a3
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-05 18:46:09 -0700

    update

--- leetcode/530. Minimum Absolute Difference in BST.js
+++ leetcode/530. Minimum Absolute Difference in BST.js
@@ -1,11 +1,34 @@
 /*
 530. Minimum Absolute Difference in BST
-
+Note: There are at least two nodes in this BST.
 */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
 
-var getMinimumDifference = function(root) {
-  if (root == null)
-    return 0;
-  
-  let minDiff = Math.min(root.val - root.left.val, root.right.val - root.val )
+var getMinimumDifference = function (root) {
+  let state = {
+    last: Number.MIN_SAFE_INTEGER,
+    ans: Number.MAX_SAFE_INTEGER
+  }
+
+  inorderIterator(root, state);
+  return state.ans;
 };
+
+function inorderIterator(root, state) {
+  if (root == null)
+    return;
+  inorderIterator(root.left, state);
+  state.ans = Math.min(state.ans, root.val - state.last);
+  state.last = root.val;
+  inorderIterator(root.right, state);
+}
+
+let root = new TreeNode(1);
+root.right = new TreeNode(3);
+root.right.left = new TreeNode(2);
+
+let res = getMinimumDifference(root);
+console.log(res);
