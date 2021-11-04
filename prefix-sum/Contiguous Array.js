4448c3c6c1cea0695c4e8843ec6aa82543cc2200
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-11-03 18:44:15 -0700

    update

---
+++ prefix-sum/Contiguous Array.js
@@ -0,0 +1,43 @@
+/*
+525. Contiguous Array
+
+https://leetcode.com/problems/contiguous-array/
+
+Given a binary array nums, return the maximum length of a contiguous subarray with an 
+equal number of 0 and 1
+
+*/
+
+/*
+  https://www.youtube.com/watch?v=uAGt1QoAoMU&ab_channel=HuaHua
+
+  Solution 
+  Covert 0 to -1, -> sum of subarray == 0
+  use a map to track the first index of each prefix sum
+
+  when prefix i == prefix j, that means sum(i+1 , j) == 0
+  */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function (nums) {
+  if (nums.length === 0) return 0;
+
+  const pos = new Map();
+  let sum = 0;
+  let ans = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    // prefix sum
+    sum += nums[i]===1 ? 1 : -1;
+    if (sum == 0) {
+      ans = i + 1;
+    } else if (pos.has(sum)) {
+      ans = Math.max(ans, i - pos.get(sum));
+    } else {
+      pos.set(sum,i);
+    }
+  }
+  return ans;
+};
---
+++ string/Validate IP Address.js
@@ -0,0 +1,33 @@
+/*
+  468. Validate IP Address
+
+  https://leetcode.com/problems/validate-ip-address/
+*/
+
+
+/*
+  Solution: Split and check each part
+*/
+/**
+ * @param {string} IP
+ * @return {string}
+ */
+var validIPAddress = function (IP) {
+  let isDigit = (c) => c == "0" || Number(c);
+  let isAlpha = (c) => {
+    let ord = c.toLowerCase().charCodeAt(0);
+    return 97 <= ord && ord <= 102; // [a-f]
+  };
+  let isNum = (s) =>
+    (s == "0" || (1 <= Number(s) && Number(s) <= 255 && s[0] != "0")) && s.split("").every((c) => isDigit(c));
+
+  let isHex = (s) =>
+    1 <= s.length &&
+    s.length <= 4 &&
+    (Number(s) != NaN || parseInt(s, 16).toString(16) == s.toLowerCase()) &&
+    s.split("").every((c) => isDigit(c) || isAlpha(c));
+
+  const IPv4 = IP.split(".").length == 4 && IP.split(".").every((s) => isNum(s));
+  const IPv6 = IP.split(":").length == 8 && IP.split(":").every((s) => isHex(s));
+  return IPv4 ? "IPv4" : IPv6 ? "IPv6" : "Neither";
+};
