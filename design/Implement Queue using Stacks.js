2ea53a8722791a6a277b03a2ca020a857786020d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-11-05 09:47:09 -0700

    update

---
+++ design/Implement Queue using Stacks.js
@@ -0,0 +1,51 @@
+/*
+232. Implement Queue using Stacks
+
+https://leetcode.com/problems/implement-queue-using-stacks/
+*/
+
+var MyQueue = function () {
+  this.stack_out = [];
+  this.stack_in = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stack_in.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.stack_out.length === 0) {
+    while (this.stack_in.length > 0) {
+      this.stack_out.push(this.stack_in.pop());
+    }
+  }
+
+  const val = this.stack_out.pop();
+  return val;
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.stack_out.length === 0) {
+    while (this.stack_in.length > 0) {
+      this.stack_out.push(this.stack_in.pop());
+    }
+  }
+  return this.stack_out.length ? this.stack_out[this.stack_out.length - 1] : null;
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.stack_in.length === 0 && this.stack_out.length === 0;
+};
---
+++ design/Implement Stack using Queues.js
@@ -0,0 +1,60 @@
+/*
+225. Implement Stack using Queues
+https://leetcode.com/problems/implement-stack-using-queues/
+*/
+
+
+var MyStack = function() {
+    this.queue_data = [];
+    this.queue_helper = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue_data.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+  while (this.queue_data.length > 1)
+  {
+    this.queue_helper.push(this.queue_data.shift());
+  }
+  const element = this.queue_data.shift();
+  //
+  while (this.queue_helper.length > 0)
+  {
+    this.queue_data.push(this.queue_helper.shift());
+  }
+  return element;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+  while (this.queue_data.length > 1)
+  {
+    this.queue_helper.push(this.queue_data.shift());
+  }
+  const element = this.queue_data.shift();
+  this.queue_helper.push(element)
+  //
+  while (this.queue_helper.length > 0)
+  {
+    this.queue_data.push(this.queue_helper.shift());
+  }
+  return element;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+  return this.queue_data.length ===0;  
+};
