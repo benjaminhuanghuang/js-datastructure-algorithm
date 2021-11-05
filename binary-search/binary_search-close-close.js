802f44f2b96703b06306738ec4e0e087486f6c2f
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-11-05 10:29:52 -0700

    update

--- binary-search/binary_search-close-close.js
+++ binary-search/binary_search-close-close.js
@@ -1,9 +1,8 @@
 function BinarySearch2 (arr, target) {
     let from = 0
     let to = arr.length - 1
-    let mid = Math.floor((from + to)/2)
     while (from <= to) {
-        mid = Math.floor((from + to)/2)
+        let mid = Math.floor((from + to)/2)
         if (arr[mid] > target) {
             to = mid - 1
         } else if (arr[mid] < target) {
---
+++ graph/Copy List with Random Pointer.js
@@ -0,0 +1,43 @@
+/*
+138. Copy List with Random Pointer
+
+https://leetcode.com/problems/copy-list-with-random-pointer/
+
+*/
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+ var copyRandomList = function (head) {
+  if (!head) return null;
+
+  var map = new Map();
+  
+  // put <original, copied> to the map
+  let curr = head;
+  while (curr) {
+    map.set(curr, new Node(curr.val, null, null));
+    curr = curr.next;
+  }
+
+  // Copy
+  curr = head;
+  while (curr) {
+    map.get(curr).next = map.get(curr.next) || null;
+    map.get(curr).random = map.get(curr.random) || null;
+    curr = curr.next;
+  }
+
+  return map.get(head);
+};
