f3fbe2f69d3e22ae4099b8763cd2b02ba16963d7
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-11-07 16:18:59 -0800

    update

---
+++ dp/Best Time to Buy and Sell Stock III.js
@@ -0,0 +1,14 @@
+/*
+  123. Best Time to Buy and Sell Stock III
+
+  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+*/
+
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    
+};
---
+++ tree/All Possible Full Binary.js
@@ -0,0 +1,64 @@
+/*
+  894. All Possible Full Binary Trees
+
+  https://leetcode.com/problems/all-possible-full-binary-trees/
+*/
+
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var allPossibleFBT = function (n) {
+  if (n % 2 === 0) return [];
+  if (n === 1) return [TreeNode(0)];
+
+  const res = [];
+  // use i nodes as left sub tree
+  for (let i = 1; i < n; i += 2) {
+    const j = n - 1 - i; // j nodes for the right sub tree
+    const left = allPossibleFBT(i);
+    const right = allPossibleFBT(j);
+
+    for (let l of left) {
+      for (let r of right) {
+        const root = new TreeNode(0);
+        root.left = l;
+        root.right = r;
+        res.push(root);
+      }
+    }
+  }
+
+  return res;
+};
+
+/*
+  Memoization
+*/
+const amp = new Map();
+
+var allPossibleFBT = function (n) {
+  if (n % 2 === 0) return [];
+  if (n === 1) return [new TreeNode(0)];
+  if (map.has(n)) {
+    return map.get(n);
+  }
+  const res = [];
+  // use i nodes as left sub tree
+  for (let i = 1; i < n; i += 2) {
+    const j = n - 1 - i;
+    const left = allPossibleFBT(i);
+    const right = allPossibleFBT(j);
+
+    for (let l of left) {
+      for (let r of right) {
+        const root = new TreeNode(0);
+        root.left = l;
+        root.right = r;
+        res.push(root);
+      }
+    }
+  }
+  map.set(n, res);
+  return res;
+};
