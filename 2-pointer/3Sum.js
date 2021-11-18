034e2562e3debd82a9ed62911d0182158327797e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-11-18 13:06:03 -0800

    update

---
+++ 2-pointer/3Sum.js
@@ -0,0 +1,49 @@
+/*
+  15. 3Sum
+
+  https://leetcode.com/problems/3sum/
+*/
+
+/*
+https://zxi.mytechroad.com/blog/two-pointers/leetcode-15-3sum/
+选取 base number， 然后在剩余的数字里使用双指针，寻找两个数的合等于 -base
+注意去除duplicated number
+
+Solution 1: Sorting + Two pointers
+
+Time complexity: O(nlogn + n^2)
+Space complexity: O(1)
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const ans = [];
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  for (let i = 0; i < n - 2; ++i) {
+    if (nums[i] > 0)
+      // nums[i] is the base number
+      break;
+    // skip the duplicated base number
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    // double points
+    let l = i + 1;
+    let r = n - 1;
+    while (l < r) {
+      if (nums[i] + nums[l] + nums[r] == 0) {
+        ans.push([nums[i], nums[l++], nums[r--]]);
+        // skip the duplicated base number !!!
+        while (l < r && nums[l] == nums[l - 1]) ++l;
+        while (l < r && nums[r] == nums[r + 1]) --r;
+      } else if (nums[i] + nums[l] + nums[r] < 0) {
+        ++l;
+      } else {
+        --r;
+      }
+    }
+  }
+  return ans;
+};
---
+++ tree/Serialize and Deserialize Binary Tree.js
@@ -0,0 +1,65 @@
+/*
+  297. Serialize and Deserialize Binary Tree
+
+  https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+  let res = [];
+
+  const preorder = (root) => {
+    if (root === null) {
+      res.push("#");
+      return;
+    }
+    res.push(root.val);
+    preorder(root.left, res);
+    preorder(root.right, res);
+  };
+
+  preorder(root, res);
+
+  return res.join(",");
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+  const arr = data.split(",");
+
+  let index = 0;
+  const helper = (data) => {
+    if (index > data.length || data[index] === "#") {
+      return null;
+    }
+    let root = new TreeNode(parseInt(data[index]));
+    index++;
+    root.left = helper(data);
+    index++;
+    root.right = helper(data);
+    return root;
+  };
+  return helper(arr);
+};
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
