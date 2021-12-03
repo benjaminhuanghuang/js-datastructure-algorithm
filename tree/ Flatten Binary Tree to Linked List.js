1222e047e61357f895d1df387d13582288bd6274
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-12-02 16:29:56 -0800

    update

--- tree/ Flatten Binary Tree to Linked List.js
+++ tree/ Flatten Binary Tree to Linked List.js
@@ -1,25 +1,38 @@
-
 /*
   114. Flatten Binary Tree to Linked List
 
   https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+/*
+  https://www.youtube.com/watch?v=NHdrzNpt1ZI
+
+  1. set right sub tree to the leaf of left tree
+  2. set left sub tree to right
+  3. set left = null
+*/
+
 /**
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
  var flatten = function(root) {
     if(!root) return root;
+    let prev = null;
 
+    flatten(root.left);
+    flatten(root.right);
 
-    root.right
+    if(root.left == null)
+    {
+      return;
+    }
+
+    let node = root.left;
+    while(node.right){
+      node = node.right;
+    }
+    node.right = root.right;
+    root.right = root.left;
+    root.left = null;
 };
