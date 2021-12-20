4a3211439ca2861586a3fd0d24159edc420eb567
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-12-19 21:39:24 -0800

    update

---
+++ dp/Maximum Length of Subarray With Positive Product.js
@@ -0,0 +1,8 @@
+/*
+1567. Maximum Length of Subarray With Positive Product
+
+Level: Medium
+
+https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product
+*/
+
--- dp/Maximum Product Subarray.js
+++ dp/Maximum Product Subarray.js
@@ -4,6 +4,29 @@
 https://leetcode.com/problems/maximum-product-subarray/
 */
 
+/*
+https://www.youtube.com/watch?v=gwZm6mIYDfk&list=PLLuMmzMTgVK6krji67w8tEAAud71nQQFe&index=4&ab_channel=HuaHua
+
+*/
+
+var maxProduct = function (nums) {
+  // return [min, max] of subarray product ends with nums[i]
+  const dp = (i) => {
+    if (i == 0) {
+      return [nums[i], nums[i]];
+    }
+    let [min, max] = dp(i - 1);
+    if (nums[i] < 0) {
+      [min, max] = [max, min];
+    }
+    return [Math.min(min * nums[i], nums[i]), Math.max(max * nums[i], nums[i])];
+  };
+  let ans = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < nums.length; i++) {
+    ans = Math.max(ans, dp(i)[1]);
+  }
+  return ans;
+};
 
 /*
 https://www.youtube.com/watch?v=XIk4fu7wjvc
@@ -14,23 +37,23 @@ https://www.youtube.com/watch?v=XIk4fu7wjvc
  * @param {number[]} nums
  * @return {number}
  */
- var maxProduct = function(nums) {
-    let maxSoFar = nums[0], minSoFar = nums[0];
-    let res = nums[0];
+var maxProduct = function (nums) {
+  let maxSoFar = nums[0],
+    minSoFar = nums[0];
+  let res = nums[0];
 
-    for(let i =1;i < nums.length; i++){
-      maxSoFar *= nums[i];
-      minSoFar *= nums[i];
-      if(nums[i] < 0) {
-        const tmp = maxSoFar;
-        maxSoFar = minSoFar;
-        minSoFar=tmp;
-      }
-      maxSoFar = Math.max(maxSoFar, nums[i]);
-      minSoFar = Math.min(minSoFar, nums[i]);
-
-      res = Math.max(res, maxSoFar);
+  for (let i = 1; i < nums.length; i++) {
+    maxSoFar *= nums[i];
+    minSoFar *= nums[i];
+    if (nums[i] < 0) {
+      const tmp = maxSoFar;
+      maxSoFar = minSoFar;
+      minSoFar = tmp;
     }
-    return res;
+    maxSoFar = Math.max(maxSoFar, nums[i]);
+    minSoFar = Math.min(minSoFar, nums[i]);
 
+    res = Math.max(res, maxSoFar);
+  }
+  return res;
 };
