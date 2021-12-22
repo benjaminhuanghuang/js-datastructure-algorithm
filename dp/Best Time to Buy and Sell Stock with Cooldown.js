02c98b3152202f98ffcc52d1311d999ae80b15ed
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-12-21 19:00:47 -0800

    fix delete treaties

--- dp/Best Time to Buy and Sell Stock with Cooldown.js
+++ dp/Best Time to Buy and Sell Stock with Cooldown.js
@@ -7,6 +7,36 @@
 */
 
 /*
+https://www.youtube.com/watch?v=xXIs_bF8lHE&t=396s&ab_channel=HuaHua
+
+
+*/
+
+var maxProfit = function (prices) {
+   /*
+    return 
+    sold: max profit after sold without holding, can't buy, can't sell
+    holdingg: max profit whith holding, Can't buy, can sell
+    cooldown: max profit whith holding, Can buy, can't sell
+
+   */
+    const dp = () =>{
+      if(i < 0) return [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, 0];
+
+      const [sold, holding, cooldown] = dp(i-1)
+
+      const a= holding + prices[i]; // sold
+      const b = Math.max(holding, cooldown - prices[i]);  // do nothing, or buy
+      const c = Math.max(cooldown, sold);
+
+      return [a, b, c];
+    }
+
+    return Math.max(...dp(prices.lenngth -1));
+};
+
+/*
+
   https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-309-best-time-to-buy-and-sell-stock-with-cooldown/
   
   
@@ -28,15 +58,15 @@
  * @param {number[]} prices
  * @return {number}
  */
- var maxProfit = function(prices) {
+var maxProfit = function (prices) {
   let sold = 0;
   let rest = 0;
   let hold = Number.MAX_SAFE_INTEGER;
   for (const price of prices) {
-      let prev_sold = sold;
-      sold = hold + price;
-      hold = Math.max(hold, rest - price);
-      rest = Math.max(rest, prev_sold);
+    let prev_sold = sold;
+    sold = hold + price;
+    hold = Math.max(hold, rest - price);
+    rest = Math.max(rest, prev_sold);
   }
   return Maht.max(rest, sold);
 };
---
+++ dp/Best Time to Buy and Sell Stock with Transaction Fee.js
@@ -0,0 +1,23 @@
+/*
+714. Best Time to Buy and Sell Stock with Transaction Fee
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+*/
+
+
+
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+ var maxProfit = function(prices, fee) {
+   /*
+    return 
+    sold: max profit afterr sold
+    holdingg: max profit whith holding
+   */
+    const dp = () =>{
+      if(i < 0) return [0, Number.MIN_SAFE_INTEGER];
+    }
+};
