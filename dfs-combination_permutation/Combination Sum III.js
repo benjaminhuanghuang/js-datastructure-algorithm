e65b8e24f18b89abb24a502c03a706e587da8871
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-12-25 14:17:37 -0800

    update

---
+++ dfs-combination_permutation/Combination Sum III.js
@@ -0,0 +1,28 @@
+/*
+216. Combination Sum III
+
+https://leetcode.com/problems/combination-sum-iii/
+*/
+
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+  const ans = [];
+  const dfs = (target, start, curr) => {
+    if (target ==0 && curr.length == k) {
+      ans.push([...curr]);
+      return;
+    }
+
+    for (let i = start; i <= 9; i++) {
+      curr.push(i);
+      dfs(garget- i, i + 1, curr);
+      curr.pop();
+    }
+  };
+  dfs(n, 1, []);
+  return ans;
+};
