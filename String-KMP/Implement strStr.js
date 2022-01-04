25e24f1793e2259a51f737776070349d0e1cfa18
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-03 16:38:04 -0800

    update

--- String-KMP/Implement strStr.js
+++ String-KMP/Implement strStr.js
@@ -4,7 +4,6 @@
 https://leetcode.com/problems/implement-strstr/
 */
 
-
 /* --------------------------------------------
 Solution KMP
 Longest prefix suffix
@@ -19,19 +18,54 @@ Space complexity: O(1)
  * @param {string} needle
  * @return {number}
  */
- var strStr = function(haystack, needle) {
-  const l1 = haystack.length();
-  const l2 = needle.length();
-  for (let i = 0; i <= l1 - l2; ++i)
-  {
-    let j = 0;
-    while (j < l2 && haystack[i + j] == needle[j])
-      ++j; 
-    if (j == l2)
-      return i;
+var strStr = function (haystack, needle) {
+  // edge case: substring="" -> 0
+  if (needle === "") return 0;
+  const prefixTable = buildPrefixTable(needle);
+  let i = 0; // location in string
+  let j = 0; // location in substring
+  while (i < haystack.length && j < needle.length) {
+    if (haystack[i] === needle[j]) {
+      // if characters match, we can move to check next characters
+      i += 1;
+      j += 1;
+    } else if (j > 0) {
+      // when characters do not match, and we have a repeating
+      // suffix-prefix pair, we still need to check after the prefix
+      j = prefixTable[j - 1];
+    } else {
+      // if characters do no match, and no repetition, we can move on
+      i += 1;
+    }
   }
-  return -1;
+  return j === needle.length ? i - j : -1;
 };
+
+function buildPrefixTable(s) {
+  const table = [0];
+  let i = 1; // location in s
+  let j = 0; // length of repeating prefix and suffix
+  while (i < s.length) {
+    // If characters match, then the repeating prefix-suffix pair
+    // gets longer by 1 character
+    if (s[i] === s[j]) {
+      j += 1;
+      table[i] = j;
+      i += 1;
+    } else if (j > 0) {
+      // If the characters do no match, and we have repetition
+      // in suffix and prefix, we still need to check
+      // the character after the prefix
+      j = table[j - 1];
+    } else {
+      // When the characters don't match and no repeating
+      // suffix-prefix pair, then we can move on
+      table[i] = 0;
+      i += 1;
+    }
+  }
+  return table;
+}
 
 /*
 Brute force
@@ -44,16 +78,13 @@ Space complexity: O(1)
  * @param {string} needle
  * @return {number}
  */
- var strStr = function(haystack, needle) {
+var strStr = function (haystack, needle) {
   const l1 = haystack.length();
   const l2 = needle.length();
-  for (let i = 0; i <= l1 - l2; ++i)
-  {
+  for (let i = 0; i <= l1 - l2; ++i) {
     let j = 0;
-    while (j < l2 && haystack[i + j] == needle[j])
-      ++j;
-    if (j == l2)
-      return i;
+    while (j < l2 && haystack[i + j] == needle[j]) ++j;
+    if (j == l2) return i;
   }
   return -1;
 };
