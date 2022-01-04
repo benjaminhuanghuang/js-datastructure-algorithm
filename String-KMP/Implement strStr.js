b4efc0ddaa5de7b128e55c4e7fc4e7de79ae881d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-03 17:05:23 -0800

    update

--- String-KMP/Implement strStr.js
+++ String-KMP/Implement strStr.js
@@ -9,6 +9,9 @@ Solution KMP
 Longest prefix suffix
 https://www.youtube.com/watch?v=JoF0Z7nVSrA&ab_channel=NeetCode
 
+https://www.youtube.com/watch?v=mAqsI9Jw1RI&ab_channel=PheezxCoding
+
+
 Time complexity: O(m+n)
 Space complexity: O(1)
 ----------------------------------------------*/
@@ -19,7 +22,7 @@ Space complexity: O(1)
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  // edge case: substring="" -> 0
+  // edge case: needle="" -> 0
   if (needle === "") return 0;
   const prefixTable = buildPrefixTable(needle);
   let i = 0; // location in string
@@ -44,24 +47,27 @@ var strStr = function (haystack, needle) {
 function buildPrefixTable(s) {
   const table = [0];
   let i = 1; // location in s
-  let j = 0; // length of repeating prefix and suffix
+  let len = 0; // length of repeating prefix and suffix
   while (i < s.length) {
     // If characters match, then the repeating prefix-suffix pair
     // gets longer by 1 character
-    if (s[i] === s[j]) {
-      j += 1;
-      table[i] = j;
+    if (s[i] === s[len]) {
+      len += 1;
+      table[i] = len;
       i += 1;
-    } else if (j > 0) {
-      // If the characters do no match, and we have repetition
-      // in suffix and prefix, we still need to check
-      // the character after the prefix
-      j = table[j - 1];
     } else {
-      // When the characters don't match and no repeating
-      // suffix-prefix pair, then we can move on
-      table[i] = 0;
-      i += 1;
+      if (len > 0) {
+        // If the characters do no match, and we have repetition
+        // in suffix and prefix, we still need to check
+        // the character after the prefix
+        len = table[len - 1];
+      } else {
+        // len == 0
+        // When the characters don't match and no repeating
+        // suffix-prefix pair, then we can move on
+        table[i] = 0;
+        i += 1;
+      }
     }
   }
   return table;
@@ -79,8 +85,11 @@ Space complexity: O(1)
  * @return {number}
  */
 var strStr = function (haystack, needle) {
+  if (needle === "") return 0;
+
   const l1 = haystack.length();
   const l2 = needle.length();
+
   for (let i = 0; i <= l1 - l2; ++i) {
     let j = 0;
     while (j < l2 && haystack[i + j] == needle[j]) ++j;
