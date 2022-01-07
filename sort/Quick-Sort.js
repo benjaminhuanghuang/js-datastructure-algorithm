2688a48380e564c7be98ff06cfda1a6a2c49ef89
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-07 15:37:50 -0800

    update

--- sort/Quick-Sort.js
+++ sort/Quick-Sort.js
@@ -1,6 +1,42 @@
 /*
   
 */
-function quickSort(array) {
+// quick sort
+/**
+ * @param A an integer array
+ * @return void
+ */
+function quickSort(A, start, end) {
+  if (start >= end) {
+    return;
+  }
 
+  let left = start,
+    right = end;
+  // key point 1: pivot is the value, not the index
+  let pivot = A[(start + end) / 2];
+
+  // key point 2: every time you compare left & right, it should be
+  // left <= right not left < right
+  while (left <= right) {
+    while (left <= right && A[left] < pivot) {
+      left++;
+    }
+    while (left <= right && A[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      const temp = A[left];
+      A[left] = A[right];
+      A[right] = temp;
+
+      left++;
+      right--;
+    }
+  }
+
+  quickSort(A, start, right);
+  quickSort(A, left, end);
 }
+
+quickSort(A, 0, A.length - 1);
--- tree-bst/Convert Binary Search Tree to Sorted Doubly Linked List.js
+++ tree-bst/Convert Binary Search Tree to Sorted Doubly Linked List.js
@@ -2,7 +2,58 @@
 426. Convert Binary Search Tree to Sorted Doubly Linked List
 
 https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
+https://wentao-shao.gitbook.io/leetcode/binary-tree/426.convert-binary-search-tree-to-sorted-doubly-linked-list
 
-https://jimmy-shen.medium.com/leetcode-426-convert-binary-search-tree-to-sorted-doubly-linked-list-5f66b3a143a8
+Convert a Binary Search Tree to a sorted Circular Doubly-Linked List in place.
+
+You can think of the left and right pointers as synonymous to the predecessor and successor 
+pointers in a doubly-linked list. For a circular doubly linked list, the predecessor of the 
+first element is the last element, and the successor of the last element is the first element.
+
+We want to do the transformation in place. After the transformation, the left pointer of the 
+tree node should point to its predecessor, and the right pointer should point to its successor. 
+You should return the pointer to the smallest element of the linked list.
+
+
 
 */
+class Node {
+  
+
+}
+/*
+  BST, 优先考虑 in-order
+*/
+/**
+ * treeToDoublyList
+ *
+ * @param root: root of a tree
+ * @return: head node of a doubly linked list
+ */
+function treeToDoublyList(root) {
+
+  // prev is very importent!
+  const inorder = (curr)=>{
+    if (curr == null) return;
+    inorder(curr.left);
+
+    prev.right = curr;
+    curr.left = prev;
+    prev = cur;
+
+    inorder(curr.right);
+  }
+
+  if(root == null) {
+    return null;
+  }
+  let prev = null;
+  const dummy = new Node(0, null, null);
+  prev = dummy;
+
+  inorder(root);
+  // connect head and tail
+  prev.right = dummy.right;
+  dummy.right.left = prev;
+  return dummy.right;
+}
