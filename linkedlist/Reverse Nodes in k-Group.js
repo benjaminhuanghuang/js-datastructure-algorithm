4458365070bc770fd43c02e6ba0c4d2c359f60eb
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-09 20:40:57 -0800

    update

--- linkedlist/Reverse Nodes in k-Group.js
+++ linkedlist/Reverse Nodes in k-Group.js
@@ -1,5 +1,8 @@
 /*
   25. Reverse Nodes in k-Group
+  
+  [Hard]
+
   https://leetcode.com/problems/reverse-nodes-in-k-group/
 */
 
@@ -10,6 +13,59 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+/*
+  Solution: Two passes.
+
+  https://www.youtube.com/watch?v=1UOPsfP85V4&ab_channel=NeetCode
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+  if (!head || k == 1) return head;
+
+  const dummy = new ListNode(-1, head);
+  const groupPrev = dummy;
+
+  // get the Kth node after curr
+  const getKth = (curr, K) => {
+    while (curr && K > 0) {
+      curr = curr.next;
+      K--;
+    }
+    return curr;
+  };
+
+  while (true) {
+    // groupPrev is not a part of group
+    let kth = getKth(groupPrev, k);
+    if (!kth) {
+      break;
+    }
+    // save the node after the group, it will be the end point of reverse
+    let groupNext = kth.next;
+
+    // revese the node in group from [groupPrev.next to kth], groupPrev is not a part of group
+    let prev = kth.next;
+    let curr = groupPrev.next;  // first node in the group
+    while (curr != groupNext) {
+      let tmp = curr.next;
+      curr.next = prev;
+      prev = curr; 
+      curr = tmp;
+    }
+    let tmp = groupPrev.next;  // first of next group
+    groupPrev.next = kth;  // make the kth to the first node in the group
+    groupPrev = tmp;
+  }
+
+  return dummy.next;
+};
 
 /*
   Solution: Two passes.
@@ -32,7 +88,7 @@ var reverseKGroup = function (head, k) {
   const dummy = new ListNode(-1, head);
 
   // get length
-  let len = 1;  // not 0
+  let len = 1; // not 0
   while ((head = head.next)) len++;
 
   // reverse
