06ab4fee6a71d42e323a02e7d9459b62d5847e45
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-15 14:50:12 -0800

    update

--- Array-2-pointers/Sort Colors.js
+++ Array-2-pointers/Sort Colors.js
@@ -4,29 +4,34 @@
   https://leetcode.com/problems/sort-colors/
 */
 
-
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var sortColors = function(nums) {
-    if(nums.length <=1)
-      return;
-      
-    let left =0, right =nums.length-1;
+var sortColors = function (nums) {
+  if (nums.length <= 1) return;
 
-    for(let i =0 ;i < nums.length ;i ++)
-    {
-      if (nums[i] == 0)
-      {
-        [nums[i],nums[left]] = [nums[left],nums[i]];
-        left ++;
-      }
-      else if (nums[i] == 2 && i < right)
-      {
-        [nums[i],nums[right]] = [nums[right],nums[i]];
-        right --;
-        i--;
-      }
+  let left = 0,
+    i = 0,
+    right = nums.length - 1;
+
+  const swap = (nums, i, j) => {
+    const tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  };
+
+  while (i < nums.length) {
+    if (nums[i] == 0) {
+      swap(nums, i, left);
+      left++;
+      i++;
+    } else if (nums[i] == 2 && i < right) {
+      swap(nums, i, right);
+      right--;
+      // 注意 此时要再次判断 nums[i]的值，因此不能i++
+    } else {
+      i++;
     }
+  }
 };
--- intervals/Insert Interval.js
+++ intervals/Insert Interval.js
@@ -18,19 +18,21 @@ var insert = function (intervals, newInterval) {
   // collect the intervals.end < newInterval.start
   for (let i = 0; i < intervals.length; i++) {
     if (newInterval[1] < intervals[i][0]) {
+      // newInterval 在 intervals[i] 前面
       res.push(newInterval);
       while (i < intervals.length) {
         res.push(intervals[i++]);
       }
       return res;
     } else if (newInterval[0] > intervals[i][1]) {
+      // newInterval 在 intervals[i] 后面
       res.push(intervals[i]);
     } else {
-      // newInterval overloap with intervals[i]
+      // newInterval overloap with intervals[i], expend newInterval
       newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])];
     }
   }
-  // for loop 退出，说明 newInterval一直有overlap
+  // for loop 退出，说明 newInterval一直有overlap, 否则会在loop 中return
   res.push(newInterval);
   return res;
 };
