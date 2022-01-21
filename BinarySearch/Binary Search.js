3594d17b73fd82de809b03bc5f8cf74e6361322c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-20 17:39:09 -0800

    update

--- BinarySearch/Binary Search.js
+++ BinarySearch/Binary Search.js
@@ -19,12 +19,31 @@ var search = function (nums, target) {
     r = nums.length;
   while (l < r) {
     let m = l + Math.floor((r - l) / 2);
-    // Find the first pos, nums[i] >=target;
+    // Find the first nums[i] >=target;
     if (nums[m] >= target) {
       r = m;
     } else {
       l = m + 1;
     }
   }
-  return l;
+  return nums[l] == target? l : -1;
+};
+/*
+  Solution 2: Check == in the loop,
+*/
+var search = function (nums, target) {
+  let l = 0,
+    r = nums.length;
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if(nums[m] == target)
+      return m;
+    // Find the first nums[i] >target;
+    if (nums[m] > target) {
+      r = m;
+    } else {
+      l = m + 1;
+    }
+  }
+  return -1;
 };
