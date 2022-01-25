3167a26929fb8c284957054ea483a1d723e570c6
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-01-24 20:47:08 -0800

    update

--- dp/Climbing Stairs.js
+++ dp/Climbing Stairs.js
@@ -17,9 +17,7 @@ var climbStairs = function (n) {
 };
 
 /*
-  Climbing Stairs - Dynamic Programming - Leetcode 70 - Python
-
-  Memoization
+  Recursion + Memoization
 */
 const m = new Map();
 m.set(0, 1);
